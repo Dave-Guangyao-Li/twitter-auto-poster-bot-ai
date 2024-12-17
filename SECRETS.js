05d4b28f5272ff355e317b6b1bd3587dbdeef18f
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2024-12-17 15:19:25 -0800

    update secrets

--- SECRETS.js
+++ SECRETS.js
@@ -1,18 +1,21 @@
 //? It is advisable to use environment variables instead of directly putting secrets in repository file but I have skipped this part as it would become complicated for many.
 //? Alternatively you can download the repository instead or forking and upload it from your account and keep it private, in that way, your secrets will not be exposed to the public.
+require("dotenv").config(); // Load environment variables from .env file
 
-const APP_KEY = "YOUR_TWITTER_APP_KEY";
-const APP_SECRET = "YOUR_TWITTER_APP_SECRET";
-const ACCESS_TOKEN = "YOUR_TWITTER_ACCESS_TOKEN";
-const ACCESS_SECRET = "YOUR_TWITTER_ACCESS_SECRET";
-const GEMINI_API_KEY = "YOUR_GEMINI_API_KEY";
+// const APP_KEY = "YOUR_TWITTER_APP_KEY";
+// const APP_SECRET = "YOUR_TWITTER_APP_SECRET";
+// const ACCESS_TOKEN = "YOUR_TWITTER_ACCESS_TOKEN";
+// const ACCESS_SECRET = "YOUR_TWITTER_ACCESS_SECRET";
+// const GEMINI_API_KEY = "YOUR_GEMINI_API_KEY";
 
 const SECRETS = {
-  APP_KEY,
-  APP_SECRET,
-  ACCESS_TOKEN,
-  ACCESS_SECRET,
-  GEMINI_API_KEY,
+  APP_KEY: process.env.APP_KEY,
+  APP_SECRET: process.env.APP_SECRET,
+  ACCESS_TOKEN: process.env.ACCESS_TOKEN,
+  ACCESS_SECRET: process.env.ACCESS_SECRET,
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY,
 };
 
 module.exports = SECRETS;
+
+export default SECRETS;
